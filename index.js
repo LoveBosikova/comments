759352502e0a02585bff2f87e7ae657126d6a07e
Bosikova Liubov Sergeevna <gulidova06@mail.ru>
2023-05-20 00:12:28 +0300

    almost done

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-
 const form = document.getElementById('form1');
-
-
-const nameInput = document.getElementById('name');
-const button = document.getElementById('button-blue');
-let name = nameInput.value;
-
-const sendComment = (name, url = 'https://yandex.ru/images/search?img_url=https%3A%2F%2Fsun9-45.userapi.com%2Fimpg%2FUxgrTraz0nkYojwMX7u05Nb6lgPBx76Af5BLCA%2Fm0qujw7FsLo.jpg%3Fsize%3D980x980%26quality%3D95%26sign%3D5eb7602756de17bcc4f75b8696180e36%26c_uniq_tag%3DNwB39wn3OXkcN8XzVAhooKgyd5JqE8-Ce2zW9Elz69s%26type%3Dalbum&lr=10716&pos=4&rpt=simage&source=serp&text=%D0%B8%D0%BA%D0%BE%D0%BD%D0%BA%D0%B0%20%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8F', comment = 'User hasnt sent any message') => {
-    console.log(name);
-    console.log(url);
+//Edit name
+const editName = (str) => {
+    let rightCaseName = str.trim();
+    rightCaseName = (rightCaseName[0].toUpperCase() + rightCaseName.slice(1).toLowerCase());
+    return rightCaseName;
 };
 
-form.onsubmit = sendComment;
+//Censor comment
+const censor = (str) => {
+
+        const words = str.trim().split(' ');
+        const cenzoredWords = [];
+
+        for (let word of words) {
+            if (word.toLowerCase() === 'viagra') {
+                cenzoredWords.push('***');
+            }
+            else if (word.toLowerCase() === 'xxx') {
+                cenzoredWords.push('***')
+            }
+            else {
+                cenzoredWords.push(word);
+            }
+        }
+        const censoredComment = cenzoredWords.join(' ');
+        return censoredComment;
+};
+
+// Create comment html structure
+
+const createHTML = (userName, link, text) => {
+    const htmlElements = [];
+    htmlElements.push('<div class="comment">');
+
+    let srcSrtring = `<img src="${link}" alt="userLogo">`;
+    htmlElements.push(srcSrtring);
+    htmlElements.push(`<div class="text">`)
+    let userNameString = `<strong class='userHeader'>${userName}</p>`;
+    htmlElements.push(userNameString);
+    let commentString =`<p class='comment-content'>${text}</p>`;
+    htmlElements.push(commentString);
+    htmlElements.push(`</div></div>`);
+
+    return htmlElements.join('');
+};
+//структура комментария
+// <div class="comment"> есть в массиве
+//   <img src="" alt="userLogo"> есть
+//   <div class="text"> есть 
+//     <strong class='userHeader'></p>усть 
+//     <p class='comment-content'></p>tcnm
+//   </div>
+// </div>
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const name = formData.get('name');
+    const url = formData.get('url');
+    const comment = formData.get('comment'); 
+    const comments = document.getElementById('comments');
+
+    //Its an edited name
+    const editedName = editName(name);
+    const censoredComment = censor(comment);
+    const newCommentHTML = createHTML(editedName, url, censoredComment);
+    comments.innerHTML = newCommentHTML;
+    console.log(comments.innerHTML);
+
+});
